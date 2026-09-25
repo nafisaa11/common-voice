428bc336147e76fbc9e266706bbba1ac85be35c8
Jauharotun Nafisah <jnafisah50@gmail.com>
2026-09-25 12:27:48 +0700

    feat(config): add FORCE_IMPORT_SENTENCES option and update import sentence logic

--- server/src/server.ts
+++ server/src/server.ts
@@ -275,7 +275,10 @@ export default class Server {
       // We no longer need to import sentences from files since users can now
       // directly add sentences on the CV platform. However, it is still
       // valuable to set up a local development environment.
-      if ('local' == getConfig().ENVIRONMENT && getConfig().IMPORT_SENTENCES) {
+      if (
+            ('local' == getConfig().ENVIRONMENT || getConfig().FORCE_IMPORT_SENTENCES) &&
+            getConfig().IMPORT_SENTENCES
+          ) {
         const import_languages = getConfig().IMPORT_LANGUAGES
         await importSentences(
           await this.model.db.mysql.createPool(),
